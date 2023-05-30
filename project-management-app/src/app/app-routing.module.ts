2109acb9eb3c2c63d21f0c3730eebdf62f6c5bb0
Ekaterina Dronova <katedronova96@gmail.com>
2023-05-31 01:27:21 +0300

    feat: add welcome page

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: 'welcome', component: WelcomePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
